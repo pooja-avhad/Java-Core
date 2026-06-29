d887f133040660f8f6b429af5f2c14ab2573b951
pooja-avhad <avhadpooja408@gmail.com>
2026-06-29 19:53:29 +0530

    Add Explicit Casting Program

---
+++ 02-Variables-and-Data-Types/Type-Casting/ExplicitCasting.java
@@ -0,0 +1,19 @@
+public class ExplicitCasting 
+{
+    public static void main(String[] args) 
+    {
+        double d=100.0;
+        float f = (float) d;
+        long l = (long) f;
+        int i = (int) l;
+        short s = (short) i;
+        byte b = (byte) s;
+
+        System.out.println("double:"+d);
+        System.out.println("float:"+f);
+        System.out.println("long:"+l);
+        System.out.println("int:"+i);
+        System.out.println("short:"+s);
+        System.out.println("byte:"+b);
+ }
+}
