7ed11705773404a46d340b527b7942c7edaebb46
pooja-avhad <avhadpooja408@gmail.com>
2026-06-29 19:47:34 +0530

    Add Variables program

---
+++ 02-Variables-and-Data-Types/Variable.java
@@ -0,0 +1,18 @@
+class Variable
+{
+    public static void main(String[] args) 
+    {
+        String name="pooja";
+        int age=20;
+        double percentage=80.20;
+        char grade='A';
+        boolean ispass=true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(percentage);
+        System.out.println(grade);
+        System.out.println(ispass);
+
+    }
+}
