0669d7ee003dcc33a01369c653535397ba188b75
pooja-avhad <avhadpooja408@gmail.com>
2026-06-29 19:48:59 +0530

    Add Primitive Data Type program

---
+++ 02-Variables-and-Data-Types/PrimitiveDT.java
@@ -0,0 +1,23 @@
+public class PrimitiveDT 
+{
+    public static void main(String[] args) 
+    {
+        byte mark =80;
+        short salary=2000;
+        int age=21;
+        long mobile=845911;
+        float per=87.4f;
+        double pi=3.14;
+        char grade='A';
+        boolean ispassed=true;
+
+        System.out.println("Marks="+mark);
+        System.out.println("Salary="+salary);
+        System.out.println("Age="+age);
+        System.out.println("Mobile="+mobile);
+        System.out.println("Percentage="+per);
+        System.out.println("Pi="+pi);
+        System.out.println("Grade="+grade);
+        System.out.println("IsPassed="+ispassed);
+}
+}
