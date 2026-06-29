033c4f17f0d628c4d0cfc6fea1a99f1320da25c1
pooja-avhad <avhadpooja408@gmail.com>
2026-06-29 19:54:28 +0530

    Add Literals Program

---
+++ 02-Variables-and-Data-Types/Literals.java
@@ -0,0 +1,25 @@
+public class Literals 
+{
+    public static void main(String[] args) 
+    {
+        int age=20;
+        float salary=30000f;
+        double pi=3.14;
+        char grade='A';
+        String name="pooja";
+        boolean ispassed=true;
+        String city=null;
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(pi);
+        System.out.println(grade);
+        System.out.println(name);
+        System.out.println(ispassed);
+        System.out.println(city);
+
+        System.out.println("Hello\n java");
+
+        
+    }
+}
