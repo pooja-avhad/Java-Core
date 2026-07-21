da8026ef2ae9b67952f0754627ce2aec0d00e8e4
pooja-avhad <avhadpooja408@gmail.com>
2026-07-21 09:27:57 +0530

    Added Assignment Operators program

---
+++ Operators/Assignment-Operators/AssignmentOperators.java
@@ -0,0 +1,19 @@
+public class AssignmentOperators 
+{
+    public static void main(String[] args) 
+    {
+        int number=100;
+        System.out.println("Number:"+number);
+        number +=10;
+        System.out.println("AfterNumber:"+number);
+        number -=10;
+        System.out.println("AfterNumber:"+number);
+        number *=10;
+        System.out.println("AfterNumber:"+number);
+        number /=10;
+        System.out.println("AfterNumber:"+number);
+        number %=10;
+        System.out.println("AfterNumber:"+number);
+ 
+    }
+}
