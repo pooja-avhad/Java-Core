9b2fc4ec4b85f1c223b0aca18ab2e01f699f3f72
pooja-avhad <avhadpooja408@gmail.com>
2026-09-21 12:29:09 +0530

    Add DefaultConstructor class with default constructor

---
+++ Core-Java/OOP/Constructor/DefaultConstructor.java
@@ -0,0 +1,17 @@
+/**
+ * DefaultConstructor
+ */
+public class DefaultConstructor 
+{
+
+    DefaultConstructor()
+    {
+        System.out.println("Constructor Default");
+    }
+    public static void main(String[] args) 
+    {
+        DefaultConstructor obj=new DefaultConstructor();
+
+    }
+
+}
