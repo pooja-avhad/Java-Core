e1e5576d49d55072cda59e04684dc76d1e624817
pooja-avhad <avhadpooja408@gmail.com>
2026-09-21 12:44:10 +0530

    Added super constructor example

---
+++ Core-Java/OOP/Inheritance/Inheritance/Parent.java
@@ -0,0 +1,21 @@
+public class Parent 
+{
+    Parent()
+    {
+        System.out.println("Parent Constructor");
+    }
+}
+class Child extends Parent
+
+{
+    Child()
+    {
+        super();
+        System.out.println("Child Constructor");
+    }
+    public static void main(String[] args) 
+    {
+        Child obj=new Child();
+    
+    }
+}
